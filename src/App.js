a095d975275fb768914fa71fd12a3aac8b5efc3a
Sahana B N <sahana.b1@slkgroup.com>
2024-11-26 15:42:05 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import Footer from './components/Footer';
 import Header from './components/Header';
 import Main from './components/Main';
 import Nav from './components/Nav';
+import {Routes,Route} from 'react-dom';
+import HomePage from './components/HomePage';
+import BookingPage from './components/BookingPage';
 
 
 function App() {
@@ -32,6 +35,13 @@ function App() {
     <Nav/>
     <Main/>
     <Footer/>
+
+    <Routes> 
+    <Route path="/" element={<HomePage />}></Route>
+    <Route path="/booking" element={<BookingPage />}></Route>
+</Routes>
+
+
     </>
   );
 }
